c08d93731cdbce866156b8be11d2bb4231ed8141
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2021-02-17 21:41:23 +0000

    create feff input file and directory

--- demeter_workflow/feff_inp.pl
+++ demeter_workflow/feff_inp.pl
@@ -1,29 +1,44 @@
 #!/usr/bin/perl
-
 use Demeter;
 
+# Call this process to save the result of running atoms
+# on a crystal file
 sub save_atoms{
 	my $crystal_name = shift;
-
-	# open crystal file and run atoms and feff to get the paths
-	my $atoms = Demeter::Atoms -> new(file => $crystal_name);
+	my $cf_len = length($crystal_name);
 	
-	return $atoms
+	
+	# open crystal file and run atoms to create 
+	# feff input file 
+	my $feff_file = lc($crystal_name);
+	substr($feff_file, $cf_len-4, 4) = "_feff.inp";
+	# create also the dir for feff
+	my $feff_dir = $feff_file;
+	substr($feff_dir, length($feff_dir)-4, 4) = "";
+	
+	mkdir($feff_dir, 0700) unless(-d $feff_dir);
+	
+	my $atoms = Demeter::Atoms -> new(file => $crystal_name);
+	open my $out, '>:encoding(UTF-8)', $feff_dir."/".$feff_file;
+	print {$out} $atoms->Write("feff6");
+	close $out;
 }
 
+# Call this process to read parameter 
+# and call save atoms with it.
+sub run_this{
+	# if no argument passed, show warning and use defaults
+	my $crystal_file = "FeS2.inp";
 
-my $crystal_file = "FeS2.inp";
+	if (!@ARGV) {
+		print "Need to provide argument\n";
+		print "- Crystal information file\n";
+		print "Arguments passed: $#ARGV \n";
+	}
+	else{
+		$crystal_file = $ARGV[0];
+		save_atoms($crystal_file);
+	}
+}
 
-#my $feff_data = get_feff($crystal_file);
-
-my $atoms = save_atoms($crystal_file);
-
-print $atoms->Write("feff6");
-
-my $feff_file = "feff_inp.inp";
-
-open my $out, '>:encoding(UTF-8)', $feff_file;
-
-print {$out} $atoms->Write("feff6");
-
-close $out;
+run_this();
