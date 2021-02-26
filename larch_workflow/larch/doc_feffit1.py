8a2d0f8192826a515c788a07fa9ff89494c98dbf
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2021-02-26 17:51:59 +0000

    move larch example under larch workflow

--- larch_workflow/larch/doc_feffit1.py
+++ larch_workflow/larch/doc_feffit1.py
@@ -1,20 +1,38 @@
 ## examples/feffit/doc_feffit1.lar
 
 # import needed for python:
+import larch
 from larch.io import read_ascii
 from larch.fitting import param, guess, param_group
 from larch.xafs import autobk, feffpath, feffit_transform, feffit_dataset, feffit, feffit_report
 from larch.wxlib.xafsplots import plot_chifit
+
+
+import lmfit
 
 # read data
 cu_data  = read_ascii('../xafsdata/cu_metal_rt.xdi')
 autobk(cu_data.energy, cu_data.mutrans, group=cu_data, rbkg=1.0, kw=2)
 
 # define fitting parameter group
-pars = param_group(amp    = param(1.0, vary=True),
-                   del_e0 = param(0.0, vary=True),
-                   sig2   = param(0.0, vary=True),
-                   del_r  = guess(0.0, vary=True) )
+# pars = larch.fitting.ParameterGroup
+# setattr(pars, 'amp', param(1.0, vary=True))
+# setattr(pars, 'del_e0', param(0.0, vary=True))
+# setattr(pars, 'sig2', param(0.0, vary=True))
+# setattr(pars, 'del_r', guess(0.0, vary=True))
+
+pars = larch.fitting.ParameterGroup()
+lmg_pars = lmfit.parameter.Parameters()
+pars.__params__ = lmg_pars
+setattr(pars, 'amp', param(1.0, vary=True))
+pars.__params__.add('amp', value = 1.0, vary=True) 
+setattr(pars, 'del_e0', param(0.0, vary=True))
+pars.__params__.add('del_e0', value = 0.0, vary=True) 
+setattr(pars, 'sig2', param(0.0, vary=True))
+pars.__params__.add('sig2', value = 0.0, vary=True) 
+setattr(pars, 'del_r', guess(0.0, vary=True))
+pars.__params__.add('del_r', value = 0.0) 
+
 
 # define a Feff Path, give expressions for Path Parameters
 path1 = feffpath('feffcu01.dat',
@@ -28,6 +46,10 @@ trans = feffit_transform(kmin=3, kmax=17, kw=2, dk=4, window='kaiser', rmin=1.4,
 
 # define dataset to include data, pathlist, transform
 dset = feffit_dataset(data=cu_data, pathlist=[path1], transform=trans)
+
+
+dsets = list()
+dsets.append(dset)
 
 # perform fit!
 out = feffit(pars, dset)
