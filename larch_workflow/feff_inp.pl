2c3c74fddb8b8d4b8bfe0319476403f02464b31e
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2021-03-17 18:03:44 +0000

    running atoms and feff in batch

--- larch_workflow/feff_inp.pl
+++ larch_workflow/feff_inp.pl
@@ -2,23 +2,27 @@
 use Demeter;
 
 # Call this process to save the result of running atoms
-# on a crystal file
+# on a crystal file.
+# Parameters: 
+#  - name of the crystal file
+#  - name of the feff dir for processing
+#  - name of the feff input file
 sub save_atoms{
 	my $crystal_name = shift;
+	my $feff_dir = shift; 
+	my $feff_file = shift;
 	my $cf_len = length($crystal_name);
-	
-	
-	# open crystal file and run atoms to create 
-	# feff input file 
-	my $feff_file = lc($crystal_name);
-	substr($feff_file, $cf_len-4, 4) = "_feff.inp";
-	# create also the dir for feff
-	my $feff_dir = $feff_file;
-	substr($feff_dir, length($feff_dir)-4, 4) = "";
-	
 	mkdir($feff_dir, 0700) unless(-d $feff_dir);
 	
-	my $atoms = Demeter::Atoms -> new(file => $crystal_name);
+	my $atoms = Demeter::Atoms -> new();
+	if (substr($crystal_name, -3,3 ) eq "cif"){
+		$atoms->cif($crystal_name);
+	}
+	else
+	{
+		$atoms->file($crystal_name);
+	}
+    
 	open my $out, '>:encoding(UTF-8)', $feff_dir."/".$feff_file;
 	print {$out} $atoms->Write("feff6");
 	close $out;
@@ -29,15 +33,22 @@ sub save_atoms{
 sub run_this{
 	# if no argument passed, show warning and use defaults
 	my $crystal_file = "FeS2.inp";
-
-	if (!@ARGV) {
-		print "Need to provide argument\n";
+	my $feff_wd = "FeS2_calc";
+	my $feff_if = "FeS2_feff.inp";
+    
+	if (!@ARGV or @ARGV < 2) {
+		my $arg_count = scalar @ARGV;
+		print "Need to provide 3 arguments \n";
 		print "- Crystal information file\n";
-		print "Arguments passed: $#ARGV \n";
+		print "- Path to store feff calculations\n";
+		print "- name of feff input file\n";
+		print "Arguments passed: $arg_count\n";
 	}
 	else{
 		$crystal_file = $ARGV[0];
-		save_atoms($crystal_file);
+		$feff_wd = $ARGV[1];
+		$feff_if = $ARGV[2];        
+		save_atoms($crystal_file, $feff_wd, $feff_if);
 	}
 }
 
