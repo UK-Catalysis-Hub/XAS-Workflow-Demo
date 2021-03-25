31b975854cebc44a79e63bda40ca9fb7dbe7513d
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2021-03-25 12:42:49 +0000

    added library to handle csv files

---
+++ larch_workflow/lib/handle_csv.py
@@ -0,0 +1,29 @@
+# libraries
+import csv
+
+# get the data from the csv_file, assuming first column is integer id
+def get_csv_data(input_file, id_field):
+    csv_data = {}
+    fieldnames = []
+    with open(input_file, encoding="utf8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:  
+            #print(row)
+            if fieldnames == []:
+                fieldnames = list(row.keys())
+            csv_data[int(row[id_field])]=row
+    return csv_data, fieldnames
+
+# writes data to the given file name
+def write_csv_data(values, filename):
+    fieldnames = []
+    for item in values.keys():
+        for key in values[item].keys():
+            if not key in fieldnames:
+                fieldnames.append(key)
+    #write back to a new csv file
+    with open(filename, 'w', newline='', encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for key in values.keys():
+            writer.writerow(values[key])
