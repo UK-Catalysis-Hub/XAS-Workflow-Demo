bbdc5792737250d2d34eec7ccf6b01c2a496ddbc
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2022-03-09 23:52:09 +0000

    scripted basic operations on Athena data

--- PSDI_pilot/reproduce_pub.pl
+++ PSDI_pilot/reproduce_pub.pl
@@ -53,15 +53,135 @@ sub read_athena_groups{
 			push(@athena_groups, [$fields[0],$fields[1],$fields[2]]);
 		}
 		if ($w_print eq "Y"){
-			print "***** Read $filename paths ******\n";
+			print "***** Read athena groups list from $filename ******\n";
 			print @athena_groups;
 		}
 		close(FH);
 	}
 	else{
-		print "could not find paths file $filename \n";
+		print "could not find groups file $filename \n";
 	}
 	return @athena_groups;
+}
+
+
+# read_athena_groups($file_name, $print)
+# $filename: csv file containing the paths, group names and assigned names
+# $w_print: indication to print or not during csv reading
+
+sub read_operations{
+	my $filename = $_[0];
+	my @wf_operations = ();
+	my $w_print = 'N';
+	if (-e $filename) {
+		if ($_[1]){
+			$w_print = $_[1];
+		}
+		
+		if ($w_print eq "Y"){
+			print "***** Reading operations list ******\n";
+			print "***** From file $filename ******\n";
+		}
+		open(FH, '<', $filename) or die $!;
+		my @fields = ();
+		while(<FH>){
+			my $gds_str = $_;
+			@fields = split "," , $gds_str;
+		push(@wf_operations, [$fields[0],$fields[1],$fields[2],$fields[3]]);
+		}
+		if ($w_print eq "Y"){
+			print "***** Read $filename ******\n";
+			print @wf_operations;
+		}
+		close(FH);
+	}
+	else{
+		print "could not find operations file $filename \n";
+	}
+	return @wf_operations;
+}
+
+
+# use the list of athena groups to retrieve data from the athena projects
+
+sub get_athena_data{
+	my @data_sources = @{$_[0]};
+	my @project_groups = ();
+	for my $idx (0 .. $#data_sources) {
+		my $athena_file = $data_sources[$idx][0];
+		my $athena_group = $data_sources[$idx][1];
+		my $group_name = $data_sources[$idx][2];
+		print "reading file: ", $athena_file , "\n";
+		my $prj_data = open_athena($athena_file);
+		print "getting data for group ", $athena_group, "\n";
+		my @all_groups = $prj_data  -> allnames;
+		for my $gp_idx (0 .. $#all_groups){
+			if ($all_groups[$gp_idx ] eq $athena_group){
+				my $temp_group = $prj_data-> record($gp_idx+1);
+				print "rename the group as: ", $group_name, " \n";
+				$temp_group -> set (name => $group_name);
+				$temp_group -> bkg_flatten(0);
+				$temp_group -> po -> start_plot;
+				push(@project_groups, $temp_group );
+			}
+		}
+	}
+	return @project_groups;
+}
+
+# first operation: plot normalised mu on energy
+sub plot_norm_mu_energy{
+	my @project_groups =  @{$_[0]};
+	my @op_gr =  @{$_[1]};
+	my @op_ep =  @{$_[2]};
+	my @eplot = (e_mu   => $op_ep[0], e_bkg    => int($op_ep[1]),
+			     e_norm => int($op_ep[2]), e_der    => int($op_ep[3]),
+				 e_pre  => int($op_ep[4]), e_post   => int($op_ep[5]),
+				 e_i0   => int($op_ep[6]), e_signal => int($op_ep[7]),
+				 e_markers => int($op_ep[8])
+				 );
+	for my $idx (0 .. $#op_gr){
+		$project_groups[$op_gr[$idx]] -> po -> set(@eplot);
+		$project_groups[$op_gr[$idx]] -> plot('E');
+	}
+}
+
+sub do_lcf{
+	my @project_groups =  @{$_[0]};
+	my @fit_gr =  @{$_[1]}; # fit groups: fit_group, standards 
+	my @op_ep =  @{$_[2]}; # plot parameters
+	my $lcf = Demeter::LCF -> new(space=>'nor', plot_difference=>0, plot_components=>0);
+
+	# fit groups: H2 (3), Ar (2), Air (1)
+	# standards: SnO2 (0), Sn Foil (4)
+
+	# fit H2
+	my $fit_group = $project_groups[$fit_gr[0]];
+	my @std_grps = ();
+	my $std_names = "";
+	for my $idx (1 .. $#fit_gr){
+		my $a_group = $project_groups[$fit_gr[$idx]];
+		push(@std_grps, $a_group);
+		$std_names = $std_names . "_" . $a_group -> name;
+	}
+	my $gr_name = $fit_group -> name;
+	my $save_as = 'lcf_fit_' . $gr_name . $std_names .'.dat';
+	$lcf->data($fit_group);
+	$lcf->add_many(@std_grps);
+
+	$lcf->xmin($fit_group->bkg_e0-20);
+	$lcf->xmax($fit_group->bkg_e0+60);
+	$lcf->po->set(emin=>-30, emax=>80);
+
+	$lcf -> fit -> plot -> save('lcf_fit_H2_1.dat');
+
+	$lcf -> pause;
+
+	$lcf->clean;
+
+	$lcf->po->start_plot;
+	$lcf->po->set(e_norm=>0, e_der=>1);
+	$lcf -> plot_fit;
 }
 
 # required data for figure 4 A, B, C
@@ -83,9 +203,8 @@ sub read_athena_groups{
 					 # ['..\psdi_data\pub_037\XAFS_prj\Sn K-edge\PtSn_OCH.prj', 'PtSn_OCH rebinned', 'H2'],
 					 # ['..\psdi_data\pub_037\XAFS_prj\Sn foil.prj', 'merge', 'Sn Foil'],
 					 # ['..\psdi_data\pub_037\XAFS_prj\Sn K-edge\PtSn_OC.prj', 'PtSn_OC_MERGE_CALIBRATE rebinned',  'PtSn'],
-					 # ['..\psdi_data\pub_037\XAFS_prj\Sn K-edge\PtSn_OCH_H2.prj', 'PtSn_OCH rebinned', 'H2-H2']); 
-					 
-my @data_sources = read_athena_groups("pub_037.csv", "N");
+					 # ['..\psdi_data\pub_037\XAFS_prj\Sn K-edge\PtSn_OCH_H2.prj', 'PtSn_OCH rebinned', 'H2-H2']); 					 
+my @data_sources = read_athena_groups("pub_037_athena.csv", "N");
 
 # intermediate: set parameters for plot object
 my @eplot = (e_mu => 1, e_bkg  => 0,
@@ -96,30 +215,43 @@ my @eplot = (e_mu => 1, e_bkg  => 0,
 			);
 
 # 2. read data from athena projects
-my @project_groups = ();
-for my $idx (0 .. $#data_sources) {
-	my $athena_file = $data_sources[$idx][0];
-	my $athena_group = $data_sources[$idx][1];
-	my $group_name = $data_sources[$idx][2];
-	print "reading file: ", $athena_file , "\n";
-	my $prj_data = open_athena($athena_file);
-	print "getting data for group ", $athena_group, "\n";
-	my @all_groups = $prj_data  -> allnames;
-	for my $gp_idx (0 .. $#all_groups){
-		if ($all_groups[$gp_idx ] eq $athena_group){
-			my $temp_group = $prj_data-> record($gp_idx+1);
-			print "rename the group as: ", $group_name, " \n";
-			$temp_group -> set (name => $group_name);
-			$temp_group -> po -> set(@eplot);
-			$temp_group -> bkg_flatten(0);
-			$temp_group -> po -> start_plot;
-			push(@project_groups, $temp_group );
-		}
+my @project_groups = get_athena_data(\@data_sources);
+
+# read the list of operations to be performed on the data objects
+my @operations = read_operations("pub_037_operations.csv", "N");
+
+for my $op_idx (0 .. $#operations){
+	my $op_id = $operations[$op_idx][0];
+	# groups used in the operation
+	my @op_gr = split /\|/, $operations[$op_idx][1];
+	
+	# plotting parameters for the operation
+	my @op_ep = split /\|/,  $operations[$op_idx][2];
+	# display text for the operation
+	my $op_msg = $operations[$op_idx][3];
+	print $op_msg,@op_gr,@op_ep,"\n";
+	if ($op_id == 1){
+		plot_norm_mu_energy(\@project_groups, \@op_gr, \@op_ep);
+		# reset plot after each operation
 	}
+	if ($op_id == 2){
+		plot_norm_mu_energy(\@project_groups, \@op_gr, \@op_ep);
+	}
+	if ($op_id == 3){
+		#plot_norm_mu_energy(\@project_groups, \@op_gr, \@op_ep);
+		print 'do LCF\n';
+		do_lcf(\@project_groups, \@op_gr, \@op_ep);
+	}
+	# reset plot after each operation
+	$project_groups[0] -> po -> start_plot;
+	print "Press any key to continue\n";
+	<STDIN>;
+	
 }
 # 3. plot first five on E normalised for Figure 4A 
 print "Show normalised E plot for first five groups\n";
 for my $idx (0 .. 4){
+	$project_groups[$idx] -> po -> set(@eplot);
 	$project_groups[$idx] -> plot('E');
 }
 my $option = <STDIN>;
