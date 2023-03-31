78b17deb1861c43adfec81a578699bc12ffe1f5f
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2023-03-31 17:47:58 +0100

    modified process to use pimatgem

--- larch_workflow/lib/atoms_feff.py
+++ larch_workflow/lib/atoms_feff.py
@@ -27,12 +27,51 @@ def run_atoms(crystal_f, feff_dir, feff_inp):
         result = False
     return result
 
+def inp_from_cif(crystal_f, feff_dir, feff_inp,absorbing,c_radius):
+    crystal_f = Path(cif_file)
+    c_parser = CifParser(crystal_f)
+    c_structure = c_parser.get_structures()[0]
+    header_file = Path(feff_dir,"inp_header.txt")
+    potentials_file = Path(feff_dir,"inp_potential.txt")
+    atoms_file = Path(feff_dir,"inp_atoms.txt")
+    
+    # create inp contents from cif
+    inp_header = Header.from_cif_file(cif_file, source=cif_file, comment="")
+    inp_atoms = Atoms(c_structure,absorbing_atom=absorbing, radius=c_radius)
+    inp_potential = Potential(c_structure,absorbing_atom=absorbing)
+    
+    # wirite individual inp content
+    inp_header.write_file(header_file)
+    inp_potential.write_file(potentials_file)
+    inp_atoms.write_file(atoms_file)
+    
+    feff_file = Path(feff_dir, feff_inp)
+    
+    # join files into single inp file
+    with open(feff_file, 'w' ) as result_file:
+        # write header
+        for line in open( header_file, 'r' ):
+            result_file.write( line )
+        # write RMAX
+        result_file.write("\nRMAX      "+str(float(c_radius)) + "\n")
+        # write potentials
+        result_file.write("\n")
+        for line in open( potentials_file, 'r' ):
+            result_file.write( line )
+        # write atoms
+        result_file.write("\n")
+        for line in open( atoms_file, 'r' ):
+            result_file.write( line )
+
+
 def copy_to_feff_dir(crystal_f, feff_file):
-    # create dir if it does not exist
-    feff_file.parent.mkdir(parents=True, exist_ok=True) 
     print ("copying", crystal_f.name, " to ", feff_file)
     shutil.copy(crystal_f, feff_file)
     return True
+
+def create_feff_dir(feff_dir, feff_inp):
+    feff_file = Path(feff_dir, feff_inp)
+    feff_file.parent.mkdir(parents=True, exist_ok=True) 
 
 def run_feff(input_files):
     feff_dir_list = []
@@ -43,6 +82,8 @@ def run_feff(input_files):
         feff_dir = crystal_f.name[:-4]+"_feff"
         feff_inp = crystal_f.name[:-4]+"_feff.inp"
         # if file is not .inp 
+        # create the folder for the outputs
+        create_feff_dir(feff_dir, feff_inp)
         # run atoms to generate input for feff
         print(crystal_f.name[-3:])
         if crystal_f.name[-3:] != "inp":
